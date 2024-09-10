54e909b8b8f9423624392fdfbf6cc16cc98f8c1e
Joseph Kizana <joseph.k20002@gmail.com>
2024-09-10 17:38:13 +1000

    added audio assets and configured background music to be played and looped after the intro

---
+++ Assets/Scripts/Audio Manager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip introMusic;
+    public AudioClip normalState;
+
+    private void Start()
+    {
+        PlayMusic(introMusic, false);
+        StartCoroutine(PlayNormalBackgroundMusic());
+    }
+
+    private void PlayMusic(AudioClip clip, bool loop)
+    {
+        audioSource.clip = clip;
+        audioSource.loop = loop;
+        audioSource.Play();
+    }
+
+    private System.Collections.IEnumerator PlayNormalBackgroundMusic()
+    {
+        yield return new WaitForSeconds(introMusic.length);
+
+        PlayMusic(normalState, true);
+    }
+}

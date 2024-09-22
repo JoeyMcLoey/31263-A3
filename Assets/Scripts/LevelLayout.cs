c2e8d690bb0cbb4bfc08dd20a509e55122bf3704
Joseph Kizana <joseph.k20002@gmail.com>
2024-09-23 00:06:17 +1000

    minor changes to structure and files

--- Assets/Scripts/LevelLayout.cs
+++ Assets/Scripts/LevelLayout.cs
@@ -4,10 +4,62 @@ using UnityEngine;
 
 public class LevelLayout : MonoBehaviour
 {
+    public GameObject[] tiles;
+    public int tileSize = 16;
+    int[,] levelMap =
+        {
+        {1,2,2,2,2,2,2,2,2,2,2,2,2,7},
+        {2,5,5,5,5,5,5,5,5,5,5,5,5,4},
+        {2,5,3,4,4,3,5,3,4,4,4,3,5,4},
+        {2,6,4,0,0,4,5,4,0,0,0,4,5,4},
+        {2,5,3,4,4,3,5,3,4,4,4,3,5,3},
+        {2,5,5,5,5,5,5,5,5,5,5,5,5,5},
+        {2,5,3,4,4,3,5,3,3,5,3,4,4,4},
+        {2,5,3,4,4,3,5,4,4,5,3,4,4,3},
+        {2,5,5,5,5,5,5,4,4,5,5,5,5,4},
+        {1,2,2,2,2,1,5,4,3,4,4,3,0,4},
+        {0,0,0,0,0,2,5,4,3,4,4,3,0,3},
+        {0,0,0,0,0,2,5,4,4,0,0,0,0,0},
+        {0,0,0,0,0,2,5,4,4,0,3,4,4,0},
+        {2,2,2,2,2,1,5,3,3,0,4,0,0,0},
+        {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
+        };
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ProduceLevel();
+    }
+
+    void ProduceLevel(){
+        for (int y = 0; y < levelMap.GetLength(0); y++){
+            for (int x = 0; x < levelMap.GetLength(1); x++){
+                int i = levelMap[y, x];
+                if (i != 0){
+                    Vector3 position = new Vector3(x * tileSize, -y * tileSize, 0);
+                    Instantiate(tiles[i], position, Quaternion.identity);
+                }
+            }
+        }
+        MirrorLevel();
+    }
+
+    void MirrorLevel(){
+        for (int y = 0; y < levelMap.GetLength(0); y++){
+            for (int x = 0; x < levelMap.GetLength(1); x++){
+                int tileIndex = levelMap[y, x];
+                if (tileIndex != 0){
+                    Vector3 posRight = new Vector3((levelMap.GetLength(1) - 1 - x) * tileSize, -y * tileSize, 0);
+                    Instantiate(tiles[tileIndex], posRight, Quaternion.Euler(0, 180, 0));
+                    
+                    Vector3 posBottom = new Vector3(x * tileSize, -(levelMap.GetLength(0) - 1 - y) * tileSize, 0);
+                    Instantiate(tiles[tileIndex], posBottom, Quaternion.Euler(180, 0, 0));
+                    
+                    Vector3 posBottomRight = new Vector3((levelMap.GetLength(1) - 1 - x) * tileSize, -(levelMap.GetLength(0) - 1 - y) * tileSize, 0);
+                    Instantiate(tiles[tileIndex], posBottomRight, Quaternion.Euler(180, 180, 0));
+                }
+            }
+        }
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/PacMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacMovement : MonoBehaviour
+{
+    public float speed = 2f;
+    private Vector3[] pathPoints = new Vector3[4];
+
+    private int currentTarget = 0;
+
+    void Start()
+    { 
+        pathPoints[0] = new Vector3(3, -3, 0);
+        pathPoints[1] = new Vector3(3, -6, 0);
+        pathPoints[2] = new Vector3(6, -6, 0);
+        pathPoints[3] = new Vector3(6, -3, 0);
+    }
+
+    void Update()
+    {
+        MoveAlongPath();
+    }
+
+    void MoveAlongPath(){
+        if (Vector3.Distance(transform.position, pathPoints[currentTarget]) < 0.1f){
+            currentTarget = (currentTarget + 1) % pathPoints.Length; 
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pathPoints[currentTarget], speed * Time.deltaTime);
+    }
+}

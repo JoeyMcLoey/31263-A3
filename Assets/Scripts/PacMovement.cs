8f79a1dcd3fcf31e3722c8bde3f71e09f79e3c40
Joseph Kizana <joseph.k20002@gmail.com>
2024-09-24 20:52:17 +1000

    death animation playing in the bottom left of the game view

--- Assets/Scripts/PacMovement.cs
+++ Assets/Scripts/PacMovement.cs
@@ -15,10 +15,10 @@ public class PacMovement : MonoBehaviour
 
     void Start()
     { 
-        pathPoints[0] = new Vector3(1, -1, 0);
-        pathPoints[1] = new Vector3(6, -1, 0);
-        pathPoints[2] = new Vector3(6, -5, 0);
-        pathPoints[3] = new Vector3(1, -5, 0);
+        pathPoints[0] = new Vector3(6, -1, 0);
+        pathPoints[1] = new Vector3(6, -5, 0);
+        pathPoints[2] = new Vector3(1, -5, 0);
+        pathPoints[3] = new Vector3(1, -1, 0);
 
         movementAudioSource.clip = movementClip;
         movementAudioSource.loop = true;

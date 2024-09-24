9345d1a7d9245f4bce832fd6bfe921551ff623c3
Joseph Kizana <joseph.k20002@gmail.com>
2024-09-24 19:46:44 +1000

    made it so that when he moves, the walking audio plays

--- Assets/Scripts/PacMovement.cs
+++ Assets/Scripts/PacMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PacMovement : MonoBehaviour
 {
+    public AudioSource movementAudioSource;
+    public AudioClip movementClip;
+
     public float speed = 2f;
     private Vector3[] pathPoints = new Vector3[4];
 
@@ -15,6 +18,8 @@ public class PacMovement : MonoBehaviour
         pathPoints[1] = new Vector3(6, -1, 0);
         pathPoints[2] = new Vector3(6, -5, 0);
         pathPoints[3] = new Vector3(1, -5, 0);
+
+        movementAudioSource.clip = movementClip;
     }
 
     void Update()
@@ -22,11 +27,15 @@ public class PacMovement : MonoBehaviour
         MoveAlongPath();
     }
 
-    void MoveAlongPath(){
+    private void MoveAlongPath(){
         if (Vector3.Distance(transform.position, pathPoints[currentTarget]) < 0.1f){
             currentTarget = (currentTarget + 1) % pathPoints.Length; 
         }
 
         transform.position = Vector3.MoveTowards(transform.position, pathPoints[currentTarget], speed * Time.deltaTime);
+
+        if (!movementAudioSource.isPlaying && speed > 0f){
+            movementAudioSource.Play();
+        }
     }
 }

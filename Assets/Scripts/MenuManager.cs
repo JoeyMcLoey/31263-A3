7a36a037f45be70e78f15a906e89fd2153e15221
Joseph Kizana <joseph.k20002@gmail.com>
2024-10-17 17:12:27 +1100

    finalised 50% section of assessment 4: changed the font to be moer pixelated, and changed some slight colouration of border

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void LoadLevel1(){
+        SceneManager.LoadScene("SampleScene");
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

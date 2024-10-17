d879481b2e5e1bcef7b62b44645c69812527b9c2
Joseph Kizana <joseph.k20002@gmail.com>
2024-10-17 16:16:59 +1100

    border animation complete

--- Assets/Scripts/BorderAnimation.cs
+++ Assets/Scripts/BorderAnimation.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class BorderAnimation : MonoBehaviour
 {
+    public GameObject border1;
+    public GameObject border2;
+    public float toggleInterval = 0.5f; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(ToggleBorders());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator ToggleBorders(){
+        while (true){
+            border1.SetActive(true);
+            border2.SetActive(false);
+            yield return new WaitForSeconds(toggleInterval);
+
+            border1.SetActive(false);
+            border2.SetActive(true);
+            yield return new WaitForSeconds(toggleInterval);
+        }
     }
 }
